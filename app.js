011a49b79cb9497e08f07f02addb6408b5a423ec
PerTerPro <chidaukhoe@gmail.com>
2019-06-16 18:39:13 +0700

    test deploy heroku

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 ﻿var restify = require('restify');
 var builder = require('botbuilder');
 
-var inMemoryStorage = new builder.MemoryBotStorage();
-
 var server = restify.createServer();
-server.use(restify.plugins.queryParser());
-server.listen(process.env.port || process.env.PORT || 56789, function () {
+
+// using plugin của restify 
+//http://restify.com/docs/plugins-api/#queryparser
+server.use(restify.plugins.queryParser());    
+
+server.listen(process.env.port || process.env.PORT, function () {
   console.log('%s listening to %s', server.name, server.url);
 });
-
 
 var connector = new builder.ChatConnector({
   appId: '5d40f369-ac75-4d56-986d-3a6dd43bbbeb',
   appPassword: 'wnwMNYN039$mksaVDA70_%]'
 });
 
+var inMemoryStorage = new builder.MemoryBotStorage();
+
 var bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage); // Register in memory storage;
 
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
+
 server.get('/', function (req, res, next) {
   res.send('Chào mừng bạn đến với bot của Thắng Ngô :v');
 });
@@ -37,7 +41,7 @@ bot.on('contactRelationUpdate', function (message) {
     var name = message.user ? message.user.name : null;
     var reply = new builder.Message()
       .address(message.address)
-      .text("Xin chào %s... Cảm ơn vì đã kết bạn với với tôi. Moa moa ...", name || 'there');
+      .text("Xin chào %s... Cảm ơn vì đã kết bạn với với tôi. Hô lê (mooning)  ...", name || 'there');
     bot.send(reply);
   }
 });
@@ -48,27 +52,13 @@ String.prototype.contains = function (content) {
 
 bot.dialog('/', function (session) {
   console.log(session.message.address);
-  bot.send(new builder.Message()
-  .text('Hello !')
-  .address(session.message.address));
-  if (session.message.text.indexOf('Haposoft') !== -1) {
-         
-    bot.send(new builder.Message()
-      .text('Đừng rời xa tôi, vì tôi lỡ yêu người mất rồi !')
-      .address(session.message.address));
-  } else if (session.message.text.indexOf('getid') > -1) {
-    
+  if (session.message.text.indexOf('getid') > -1) {
     bot.send(new builder.Message()
       .text('Your Skype Id: ' + session.message.address.conversation.id)
       .address(session.message.address));
   } else {
-     
     bot.send(new builder.Message()
       .text('Chúng ta không thuộc về nhau !!!')
       .address(session.message.address));
   }
-  bot.send(new builder.Message()
-  .text('GoodBye !')
-  .address(session.message.address));
-  
 });
