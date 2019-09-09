9474adbe1c4c83969185ef9cd374b1fb961a9133
PerTerPro <chidaukhoe@gmail.com>
2019-09-09 17:55:25 +0700

    fix cors

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ const server = restify.createServer();
 //http://restify.com/docs/plugins-api/#queryparser
 server.use(restify.plugins.queryParser());
 //using CORS
-server.use(restify.CORS({
-  origins: ['*'],   // defaults to ['*']
-  // credentials: true,                 // defaults to false
-  // headers: ['x-foo']                 // sets expose-headers
-}));
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
 
 server.listen(process.env.port || process.env.PORT || 56789, function () {
   console.log('%s listening to %s', server.name, server.url);
